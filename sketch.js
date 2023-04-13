8e0d5808e218aba97c4697969f7612c0cbb39167
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2023-04-13 14:29:39 -0300

    test

--- sketch.js
+++ sketch.js
@@ -1,37 +1,37 @@
 let cucas = [];
-let nCucas = 3;   //cantidad inicial de cucas
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
   colorMode(HSB,360,100,100,100);
 
-  for(let i=0; i<nCucas; i++){
-    cucas[i] = new Cuca(random(0,width), random(0,height));    //cada cuca aparece en un punto random.
-  }
+  cucas.push(new Cuca(width/2, height/2));    //primera cuca al centro
 
 }
 
 function draw() {
   background(360);
 
-  for(let i=0; i<nCucas; i++){        //métodos de cucas
+  for(let i=0; i<cucas.length; i++){        //métodos de cucas
     cucas[i].calcular();
     cucas[i].mover();
     cucas[i].dibujar();
+
+    console.log(cucas.length);
   }
 
 }
 
 function mouseClicked(){                      //al hacer clic...
-  for(let i=0; i<int(random(nCucas)); i++){  
-    cucas[i].quieto();                        //...algunas cucas empiezan a moverse y otras dejan de hacerlo
+  if(cucas.length==1){ cucas[0].quieto(); }
+  else{
+    for(let i=0; i<int(random(cucas.length)); i++){
+      cucas[int(random(cucas.length))].quieto();          //...algunas cucas empiezan a moverse y otras dejan de hacerlo
+    }
   }
-
 }
 
 function keyPressed(){    //al presionar una tecla...
-  nCucas++;
-  cucas[(nCucas-1)] = new Cuca(random(0,width), random(0,height));      //...crear una cuca nueva
-  console.log(nCucas);
+  //nCucas++;
+  cucas.push(new Cuca(random(0,width), random(0,height)));      //...crear una cuca nueva
 
 }
