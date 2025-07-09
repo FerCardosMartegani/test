b32b5d2bf8e5e783562bc781a79944728d837153
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2025-07-09 15:22:00 -0300

    Add files via upload

--- sketch2.js
+++ sketch2.js
@@ -3,7 +3,8 @@ let audios;
 let mic, amp, VAD;
 let nivelFondo = 0;
 let vozActiva = false;
-let nivelVoz = 0, nivelVozMax = 0;
+let nivelVoz = 0,
+  nivelVozMax = 0;
 let nivelDeCaos = 2;
 
 const CAMBIONIVEL = 0.1;
@@ -70,7 +71,9 @@ function draw() {
     // Diferencia entre volumen actual y fondo → voz
     nivelVoz = nivelActual - nivelFondo;
     nivelVoz = max(nivelVoz, 0); // por si es negativa
-    nivelVozMax = max(nivelVoz, nivelVozMax);
+    if (nivelVoz > nivelVozMax) {
+      nivelVozMax = nivelVoz;
+    }
     text("VOZ detectada. Nivel voz: " + nf(nivelVozMax, 1, 4), 10, 30);
   }
 
