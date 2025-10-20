06922dd4b1569eb6a88a52193048427a92557cf2
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2025-10-20 19:24:36 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+// ------------------------------------------------------------------------------------------------------DECLARACIÓN
+
+// ------------------------------------------------------------------------------------------------------SETUP
+function setup() {
+  // createCanvas(400, 400);
+
+  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    createButton('Activar sensores').mousePressed(() => {
+      DeviceOrientationEvent.requestPermission().then(response => {
+        if (response === 'granted') {
+          window.addEventListener('deviceorientation', handleRotation);
+        }
+      });
+    });
+  } else {
+    window.addEventListener('deviceorientation', handleRotation);
+  }
+}
+
+// ------------------------------------------------------------------------------------------------------DRAW
+function draw() {
+  // background(220);
+}
+
+// ------------------------------------------------------------------------------------------------------ROTATION
+function handleRotation(e) {
+  let angle = e.gamma; // rotación izquierda/derecha
+  if (Math.abs(angle - lastAngle) > 30) { // umbral
+    navigator.vibrate(100);
+    lastAngle = angle;
+  }
+}
